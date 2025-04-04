60f5e55496a6a02aee44da8c0b929f0b22bd4d98
itorly <itorly@163.com>
2025-04-04 23:55:21 +0800

    pandas dtypes attribute

--- getting_started/read_write/read write tabular data.py
+++ getting_started/read_write/read write tabular data.py
@@ -3,3 +3,7 @@ import pandas as pd
 titanic = pd.read_csv("titanic.csv")
 # titanic
 print('titanic:\n', titanic)
+
+# pandas dtypes attribute
+# titanic.dtypes
+print('\ntitanic.dtypes:\n', titanic.dtypes)
