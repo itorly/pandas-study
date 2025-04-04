0006fcf085a0783d9a43a9fca684ef778bfc169b
itorly <itorly@163.com>
2025-04-04 17:36:48 +0800

    typical functions: info(), describe(), max(), head(), tail()

--- getting_started/table_oriented/pandas data table representation.py
+++ getting_started/table_oriented/pandas data table representation.py
@@ -23,3 +23,36 @@ print('df["Age"]:\n', df["Age"])
 # create a Series from scratch
 ages = pd.Series([22, 35, 58], name="Age")
 
+
+# typical functions a DataFrame
+# df.info()
+print('\ndf.info():\n', df.info())
+
+# df.describe()
+print('\ndf.describe():\n', df.describe())
+
+# ages.max()
+print('\nages.max():\n', ages.max())
+
+# df.head()
+print('\ndf.head():\n', df.head())
+
+# df.tail()
+print('\ndf.tail():\n', df.tail())
+
+
+# typical functions a Series
+# ages.info()
+print('\nages.info():\n', ages.info())
+
+# ages.describe()
+print('\nages.describe():\n', ages.describe())
+
+# df["Age"].max()
+print('\ndf["Age"].max():\n', df["Age"].max())
+
+# ages.head()
+print('\nages.head():\n', ages.head())
+
+# ages.tail()
+print('\nages.tail():\n', ages.tail())
