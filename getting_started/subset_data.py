eaf8e8b53b3c4a536106eac70bf0a1895d305a9d
itorly <itorly@163.com>
2025-04-05 23:58:39 +0800

    type, shape, select multiple columns

--- getting_started/subset_data.py
+++ getting_started/subset_data.py
@@ -11,3 +11,13 @@ print('type_age:\n', type_age)
 
 shape_age = ages.shape
 print('shape_age:\n', shape_age)
+
+# select multiple columns
+age_sex = titanic[["Age", "Sex"]]
+print('age_sex:\n', age_sex)
+
+type_age_sex = type(titanic[["Age", "Sex"]])
+print('type_age_sex:\n', type_age_sex)
+
+shape_age_sex = titanic[["Age", "Sex"]].shape
+print('shape_age_sex:\n', shape_age_sex)
