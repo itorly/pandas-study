485e002907ed5da82a5461f5b3dedee65c5c9e48
itorly <itorly@163.com>
2025-04-05 23:25:12 +0800

    to_excel, read_excel

--- getting_started/read_write/read write tabular data.py
+++ getting_started/read_write/read write tabular data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+# pandas read_csv method
 titanic = pd.read_csv("titanic.csv")
 # titanic
 print('titanic:\n', titanic)
@@ -7,3 +8,12 @@ print('titanic:\n', titanic)
 # pandas dtypes attribute
 # titanic.dtypes
 print('\ntitanic.dtypes:\n', titanic.dtypes)
+
+# pandas to_excel method
+titanic.to_excel("titanic.xlsx", sheet_name="passengers", index=False)
+
+# pandas read_excel method
+titanic = pd.read_excel("titanic.xlsx", sheet_name="passengers")
+
+# titanic
+print('titanic:\n', titanic)
