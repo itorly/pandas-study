c0c50d4e69783e8cd0f45abfb4a8178528fc670b
itorly <itorly@163.com>
2025-04-05 23:33:37 +0800

    head function

--- getting_started/read_write/read write tabular data.py
+++ getting_started/read_write/read write tabular data.py
@@ -5,15 +5,24 @@ titanic = pd.read_csv("titanic.csv")
 # titanic
 print('titanic:\n', titanic)
 
+# read first 8 rows
+head_8 = titanic.head(8)
+print('\nhead_8:\n', head_8)
+
 # pandas dtypes attribute
 # titanic.dtypes
 print('\ntitanic.dtypes:\n', titanic.dtypes)
 
 # pandas to_excel method
-titanic.to_excel("titanic.xlsx", sheet_name="passengers", index=False)
+# titanic.to_excel("titanic.xlsx", sheet_name="passengers", index=False)
 
 # pandas read_excel method
 titanic = pd.read_excel("titanic.xlsx", sheet_name="passengers")
 
 # titanic
 print('titanic:\n', titanic)
+
+# head
+# read first 5 rows
+head = titanic.head()
+print('\nhead:\n', head)
