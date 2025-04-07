a62a6834269e3b4ee9f424682806520abfd78317
itorly <itorly@163.com>
2025-04-07 17:23:55 +0800

    rename some titles

--- getting_started/subset_data.py
+++ getting_started/subset_data.py
@@ -3,7 +3,10 @@ import pandas as pd
 print('# 3 subset data\n')
 titanic = pd.read_csv("data/titanic.csv")
 
-# 3.1 select a single column
+# 3.1 select specific columns from a DataFrame
+print('## 3.1 select specific columns from a DataFrame\n')
+
+# 3.1.1 select a single column
 print('## 3.1 select a single column\n')
 ages = titanic["Age"]
 print('ages:\n', ages)
@@ -14,7 +17,7 @@ print('type_age:\n', type_age)
 shape_age = ages.shape
 print('shape_age:\n', shape_age)
 
-# 3.2 select multiple columns
+# 3.1.2 select multiple columns
 print('## 3.2 select multiple columns\n')
 age_sex = titanic[["Age", "Sex"]]
 print('age_sex:\n', age_sex)
@@ -25,11 +28,12 @@ print('type_age_sex:\n', type_age_sex)
 shape_age_sex = titanic[["Age", "Sex"]].shape
 print('shape_age_sex:\n', shape_age_sex)
 
-# 3.3 conditional expression or function
-print('\n## 3.3 conditional expression or function\n')
+# 3.2 filter specific rows from a DataFrame
+# conditional expression or function
+print('\n## 3.2 filter specific rows from a DataFrame\n')
 
-# 3.3.1 conditional expression: greater than
-print('### 3.3.1 conditional expression: greater than\n')
+# 3.2.1 conditional expression: greater than
+print('### 3.2.1 conditional expression: greater than\n')
 # The output of the conditional expression is
 # a Series of boolean values
 age_gt_35 = titanic["Age"] > 35
@@ -41,8 +45,8 @@ print('\nabove_35:\n', above_35)
 # above_35.shape
 print('\nabove_35.shape:\n', above_35.shape)
 
-# 3.3.2 conditional expression: equal, or
-print('\n### 3.3.2 conditional expression: equal, or\n')
+# 3.2.2 conditional expression: equal, or
+print('\n### 3.2.2 conditional expression: equal, or\n')
 equal_to_2 = titanic["Pclass"] == 2
 print('equal_to_2:\n', equal_to_2)
 
@@ -53,8 +57,8 @@ head_equal_to_2_or_3 = equal_to_2_or_3.head()
 print('\nhead_equal_to_2_or_3:\n', head_equal_to_2_or_3)
 
 
-# 3.3.3 conditional function: isin()
-print('\n### 3.3.3 conditional function: isin()\n')
+# 3.2.3 conditional function: isin()
+print('\n### 3.2.3 conditional function: isin()\n')
 
 # check for which rows the Pclass column is either 2 or 3.
 isin_2_3 = titanic["Pclass"].isin([2, 3])
@@ -67,8 +71,8 @@ head_2_3 = titanic_2_3.head()
 print('\nhead_2_3:\n', head_2_3)
 
 
-# 3.3.4 conditional function: notna()
-print('\n### 3.3.4 conditional function: notna()\n')
+# 3.2.4 conditional function: notna()
+print('\n### 3.2.4 conditional function: notna()\n')
 
 notna = titanic["Age"].notna()
 print('notna:\n', notna)
@@ -81,3 +85,5 @@ print('\nhead_age_notna:\n', head_age_notna)
 
 shape_age_notna = age_notna.shape
 print('\nshape_age_notna:\n', shape_age_notna)
+
+
