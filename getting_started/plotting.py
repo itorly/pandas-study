1d84852b042a8210d9bab11f4d8fadb719e3f888
itorly <itorly@163.com>
2025-04-08 23:17:03 +0800

    4.plotting 4.1 a quick visual check of the data

---
+++ getting_started/plotting.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+air_quality = pd.read_csv("data/air_quality_no2.csv", index_col=0, parse_dates=True)
+air_quality.plot()
+plt.title('Air Quality - NO₂ Concentrations')
+plt.ylabel('NO₂ (µg/m³)')
+plt.show()
+
+
