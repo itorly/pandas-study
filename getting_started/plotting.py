7af6d6dee2b4f3edfd66ee648527447281a5c842
itorly <itorly@163.com>
2025-04-09 13:04:55 +0800

    PyCharm-Specific Fix

--- getting_started/plotting.py
+++ getting_started/plotting.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-
+# PyCharm-Specific Fix,
+# File → Settings → Tools → Python Plots → Uncheck "Show plots in tool window"
 air_quality = pd.read_csv("data/air_quality_no2.csv", index_col=0, parse_dates=True)
 air_quality.plot()
 plt.title('Air Quality - NO₂ Concentrations')
