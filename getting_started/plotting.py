04789482c3348dfab9e1f8c8096765215e49da0d
itorly <itorly@163.com>
2025-04-09 13:29:40 +0800

    4.2 plot only the columns of the data table

--- getting_started/plotting.py
+++ getting_started/plotting.py
@@ -2,10 +2,18 @@ import matplotlib.pyplot as plt
 import pandas as pd
 # PyCharm-Specific Fix,
 # File → Settings → Tools → Python Plots → Uncheck "Show plots in tool window"
+
+# 4 plotting
 air_quality = pd.read_csv("data/air_quality_no2.csv", index_col=0, parse_dates=True)
+
+# 4.1 a quick visual check of the data
 air_quality.plot()
 plt.title('Air Quality - NO₂ Concentrations')
 plt.ylabel('NO₂ (µg/m³)')
 plt.show()
 
-
+# 4.2 plot only the columns of the data table
+air_quality["station_paris"].plot()
+plt.title('Air Quality - NO₂ Concentrations')
+plt.ylabel('NO₂ (µg/m³)')
+plt.show()
