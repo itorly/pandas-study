1f9bd091196ddad5fb8bb0c19349498b121343c5
itorly <itorly@163.com>
2025-04-09 14:19:11 +0800

    4.3 scatter plot

--- getting_started/plotting.py
+++ getting_started/plotting.py
@@ -14,6 +14,12 @@ plt.show()
 
 # 4.2 plot only the columns of the data table
 air_quality["station_paris"].plot()
-plt.title('Air Quality - NO₂ Concentrations')
+plt.title('Air Quality in Paris - NO₂ Concentrations')
 plt.ylabel('NO₂ (µg/m³)')
 plt.show()
+
+# 4.3 scatter plot
+air_quality.plot.scatter(x="station_london", y="station_paris", alpha=0.5)
+plt.title('Air Quality measured in London versus Paris - NO₂ Concentrations')
+plt.show()
+
