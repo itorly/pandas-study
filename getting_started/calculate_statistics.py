cb74f89dc734b69f3e04729049935a32d5eb50b9
itorly <itorly@163.com>
2025-04-16 23:27:38 +0800

    6.1 Aggregating statistics

--- getting_started/calculate_statistics.py
+++ getting_started/calculate_statistics.py
@@ -6,18 +6,20 @@ titanic = pd.read_csv("data/titanic.csv")
 head = titanic.head()
 print('head:\n', head)
 
-# 6.1 average
+# 6.1 Aggregating statistics
+# 6.1.1 average
 age_mean = titanic["Age"].mean()
 print('age_mean:\n', age_mean)
 
-# 6.2 median
+# 6.1.2 median
 age_fare__median = titanic[["Age", "Fare"]].median()
 print('age_fare__median:\n', age_fare__median)
 
-# 6.3 describe function
+# 6.1.3 describe function
 describe = titanic[["Age", "Fare"]].describe()
 print('\ndescribe:\n', describe)
 
-# 6.4 agg function
+# 6.1.4 agg function
 agg = titanic.agg({"Age": ["min", "max", "median", "skew"], "Fare": ["min", "max", "median", "mean"], })
 print('\nagg:\n', agg)
+
