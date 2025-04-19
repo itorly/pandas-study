e532890c4f39a38a503795d92dfa353795e5cc05
itorly <itorly@163.com>
2025-04-19 23:58:42 +0800

    7.2 Long to wide table format 7.2.1 subset: ==, sort_index(), groupby()

--- getting_started/reshape_table_layout.py
+++ getting_started/reshape_table_layout.py
@@ -12,6 +12,7 @@ air_quality_head = air_quality.head()
 print('air_quality_head:\n', air_quality_head)
 
 # 7.1 Sort table rows
+print('\n## 7.1 Sort table rows\n')
 titanic_sort_values_by_age = titanic.sort_values(by="Age")
 head_of_titanic_sort_values_by_age = titanic_sort_values_by_age.head()
 print('\nhead_of_titanic_sort_values_by_age:\n', head_of_titanic_sort_values_by_age)
@@ -20,3 +21,15 @@ titanic_sort_values_by_Pclass_and_age = titanic.sort_values(by=['Pclass', 'Age']
 head_of_titanic_sort_values_by_Pclass_and_age = titanic_sort_values_by_Pclass_and_age.head()
 print('\nhead_of_titanic_sort_values_by_Pclass_and_age:\n', head_of_titanic_sort_values_by_Pclass_and_age)
 
+
+# 7.2 Long to wide table format
+print('\n## 7.2 Long to wide table format\n')
+
+# 7.2.1 subset: ==, sort_index(), groupby()
+# filter for no2 data only
+no2 = air_quality[air_quality["parameter"] == "no2"]
+
+# use 2 measurements (head) for each location (groupby)
+no2_subset = no2.sort_index().groupby(["location"]).head(2)
+
+print('\nno2_subset:\n', no2_subset)
