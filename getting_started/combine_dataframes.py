9891994f2bdfa82601e25b27a3f3d5b6ff99228a
itorly <itorly@163.com>
2025-04-23 00:03:30 +0800

    8.0.2 Air quality Particulate matter data

--- getting_started/combine_dataframes.py
+++ getting_started/combine_dataframes.py
@@ -12,3 +12,13 @@ air_quality_no2 = air_quality_no2[["date.utc", "location",
 
 air_quality_no2_head = air_quality_no2.head()
 print('air_quality_no2_head:\n', air_quality_no2_head)
+
+# 8.0.2 Air quality Particulate matter data
+air_quality_pm25 = pd.read_csv("data/air_quality_pm25_long.csv",
+                               parse_dates=True)
+
+air_quality_pm25 = air_quality_pm25[["date.utc", "location",
+                                     "parameter", "value"]]
+
+air_quality_pm25_head =air_quality_pm25.head()
+print('air_quality_pm25_head:\n', air_quality_pm25_head)
