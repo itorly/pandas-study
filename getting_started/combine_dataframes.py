7fb629c4a022ddc7ba4efc93fa8972ab5b2795bc
itorly <itorly@163.com>
2025-04-23 23:59:49 +0800

    8.1 Concatenating objects

--- getting_started/combine_dataframes.py
+++ getting_started/combine_dataframes.py
@@ -1,4 +1,5 @@
 # 8 combine_dataframes
+# How to combine data from multiple tables
 import pandas as pd
 
 # 8.0 prerequisite
@@ -22,3 +23,16 @@ air_quality_pm25 = air_quality_pm25[["date.utc", "location",
 
 air_quality_pm25_head =air_quality_pm25.head()
 print('air_quality_pm25_head:\n', air_quality_pm25_head)
+
+# 8.1 Concatenating objects
+print('\n## 8.1 Concatenating objects\n')
+# combine the measurements of NO2 and PM2.5 two tables with a similar structure, in a single table
+
+air_quality = pd.concat([air_quality_pm25, air_quality_no2], axis=0)
+
+air_quality_head = air_quality.head()
+print('air_quality_head:\n', air_quality_head)
+
+print('Shape of the ``air_quality_pm25`` table: ', air_quality_pm25.shape)
+print('Shape of the ``air_quality_no2`` table: ', air_quality_no2.shape)
+print('Shape of the resulting ``air_quality`` table: ', air_quality.shape)
