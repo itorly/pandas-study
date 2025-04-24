e9651240c6d4e254d30b72f86a09fe8d69c5a799
itorly <itorly@163.com>
2025-04-24 11:25:24 +0800

    8.1.1.1 concat() function, axis = 0 and axis = 1

--- getting_started/combine_dataframes.py
+++ getting_started/combine_dataframes.py
@@ -30,7 +30,22 @@ print('\n## 8.1 Concatenating objects\n')
 # 8.1.1 concat() function
 # combine the measurements of NO2 and PM2.5 two tables with a similar structure, in a single table
 
+# By default, concatenation is along axis 0, so the resulting table combines the rows of the input tables.
 air_quality = pd.concat([air_quality_pm25, air_quality_no2], axis=0)
+
+air_quality_axis_1 = pd.concat([air_quality_pm25, air_quality_no2], axis=1)
+print('\nair_quality_axis_1:\n', air_quality_axis_1)
+
+# 8.1.1.1 concat() function, axis = 0 and axis = 1
+df1 = pd.DataFrame({'A': [1, 2], 'B': [3, 4]})
+df2 = pd.DataFrame({'A': [5, 6], 'B': [7, 8]})
+
+result_axis_0 = pd.concat([df1, df2], axis=0)
+print('\nresult_axis_0:\n', result_axis_0)
+
+result_axis_1 = pd.concat([df1, df2], axis=1)
+print('\nresult_axis_1:\n', result_axis_1)
+
 
 air_quality_head = air_quality.head()
 print('air_quality_head:\n', air_quality_head)
