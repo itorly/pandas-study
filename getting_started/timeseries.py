044502b392419432a86a70e012326b29ae1b2117
itorly <itorly@163.com>
2025-04-26 18:40:25 +0800

    9.1 Using pandas datetime properties 9.1.1 convert to datetime 9.1.1.1 to_datetime() function 9.1.1.2 parse_dates parameter of read_csv() or read_json() function

--- getting_started/timeseries.py
+++ getting_started/timeseries.py
@@ -14,3 +14,18 @@ print('\nair_quality_head:\n', air_quality_head)
 # unique() function
 city_unique = air_quality.city.unique()
 print('\ncity_unique:\n', city_unique)
+
+# 9.1 Using pandas datetime properties
+print("\n## 9.1 Using pandas datetime properties")
+
+# 9.1.1 convert to datetime
+print("\n## 9.1.1 convert to datetime")
+# 9.1.1.1 to_datetime() function
+air_quality["datetime"] = pd.to_datetime(air_quality["datetime"])
+air_quality_datetime = air_quality["datetime"]
+print('\nair_quality_datetime:\n', air_quality_datetime)
+
+# 9.1.1.2 parse_dates parameter of read_csv() or read_json() function
+read_csv_and_convert_to_datetime = pd.read_csv("data/air_quality_no2_long.csv", parse_dates=["date.utc"])
+print('\nread_csv_and_convert_to_datetime:\n', read_csv_and_convert_to_datetime)
+
