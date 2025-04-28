188cd44fe80e0fe68fb5a12e66347e00c18b13bf
itorly <itorly@163.com>
2025-04-28 16:23:47 +0800

    10.5 replace

--- getting_started/text_data.py
+++ getting_started/text_data.py
@@ -22,6 +22,7 @@ titanic["Surname"] = titanic["Name"].str.split(",").str.get(0)
 print('\ntitanic["Surname"]:\n', titanic["Surname"])
 
 # 10.3 contains and indexing
+print('# 10.3 contains and indexing\n')
 # contains
 contains = titanic["Name"].str.contains("Countess")
 print('\ncontains:\n', contains)
@@ -30,6 +31,7 @@ contains_records = titanic[titanic["Name"].str.contains("Countess")]
 print('\ncontains_records:\n', contains_records)
 
 # 10.4 len, idmax and loc
+print('# 10.4 len, idmax and loc\n')
 # len
 len = titanic["Name"].head().str.len()
 print('\nlen:\n', len)
@@ -41,3 +43,8 @@ print('\nidxmax:\n', idxmax)
 # loc
 loc = titanic.loc[titanic["Name"].str.len().idxmax(), "Name"]
 print('\nloc:\n', loc)
+
+# 10.5 replace
+print('# 10.5 replace\n')
+titanic["Sex_short"] = titanic["Sex"].replace({"male": "M", "female": "F"})
+print('\ntitanic["Sex_short"]:\n', titanic["Sex_short"])
