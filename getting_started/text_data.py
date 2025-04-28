a5f1f884f47b635ff7c1a491f2019e4e81c81820
itorly <itorly@163.com>
2025-04-28 16:07:40 +0800

    10.2 split and get

--- getting_started/text_data.py
+++ getting_started/text_data.py
@@ -10,3 +10,13 @@ print('# 10.1 lowercase\n')
 # lower
 lower = titanic["Name"].str.lower()
 print('\nlower:\n', lower)
+
+# 10.2 split and get
+print('# 10.2 split and get\n')
+# split
+split = titanic["Name"].str.split(",")
+print('\nsplit:\n', split)
+
+# get
+titanic["Surname"] = titanic["Name"].str.split(",").str.get(0)
+print('\ntitanic["Surname"]:\n', titanic["Surname"])
